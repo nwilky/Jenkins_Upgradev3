91c88e161f818b02f86dadb67277ac85df6aa0b0
Nicholas Wilky <nwilky00@gmail.com>
2020-12-07 16:03:07 -0500

    changed the name

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -19,7 +19,7 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "Kiki";
+    String someone = "Bosworth";
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
